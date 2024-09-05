4b2444e6183e5fb7aec188efbe91bcb374258817
Sky Chin <sky4just@gmail.com>
2024-09-06 07:31:57 +0800

    Update
    Signed-off-by: Sky Chin <sky4just@gmail.com>

--- vscode/install.sh
+++ vscode/install.sh
@@ -6,10 +6,27 @@ code --install-extension justusadam.language-haskell
 code --install-extension mikestead.dotenv
 code --install-extension ms-python.python
 code --install-extension ms-vscode.sublime-keybindings
+code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension octref.vetur
 code --install-extension pnp.polacode
 code --install-extension rust-lang.rust
 code --install-extension stevejpurves.cucumber
 code --install-extension teabyii.ayu
-code --install-extension eamodio.gitlens
+
+# Elixir
 code --install-extension jakebecker.elixir-ls
+
+# Git
+code --install-extension eamodio.gitlens
+code --install-extension vivaxy.vscode-conventional-commits
+code --install-extension github.vscode-pull-request-github
+
+# Tailwind CSS
+code --install-extension heybourn.headwind
+code --install-extension bradlc.vscode-tailwindcss
+
+# CodeStream
+code --install-extension CodeStream.codestream
+
+# Laravel Extension Pack
+code --install-extension onecentlin.laravel-extension-pack
